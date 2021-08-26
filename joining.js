d7a2b285bbcaee9acf27fe28524f3c93325a7a4c
khajaumar <khajau460@gmail.com>
2021-08-26 03:58:45 -0700

    joining

---
+++ joining.js
@@ -0,0 +1,13 @@
+// // const arr= ["A", "R", "M"];
+// // console.log(arr.join(""));
+
+// const arry = ["A", "R", "M"];
+// // const text= "";
+// // arry.forEach((element) => {
+// //     text += element;
+// // });
+// // console.log(text);
+// console.log(arry.reduce((a , b)=> a + b));
+
+arr = [2, 4, 3];
+console.log(arr.reduce((a , b)=> a * b**3, 1));
