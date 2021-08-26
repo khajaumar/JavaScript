17e7f5e3581e49bf6be67e868380d22b2b94ae15
khajaumar <khajau460@gmail.com>
2021-08-26 04:14:44 -0700

    optimize

--- joining.js
+++ joining.js
@@ -14,6 +14,6 @@
 
 const arr = [null, 2, 3, 4, "cat", {num: 25}, undefined, 2];
 
-const newArr = arr.filter((item) => typeof item === "number" && item);
+const newArr = arr.filter((item) => typeof item === "number" && item).reduce((acc, num) => acc + num );
  
-console.log(newArr.reduce((acc, num) => acc + num ));
+console.log(newArr);
