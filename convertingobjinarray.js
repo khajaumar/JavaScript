9453a3434c90ae36dda88e4affa52ba013d0bd1d
khajaumar <khajau460@gmail.com>
2021-08-26 05:31:55 -0700

    objarray

--- convertingobjinarray.js
+++ convertingobjinarray.js
@@ -8,5 +8,7 @@ const obj = {
 // const newArr = keys.map((key) => obj[key]);
 // console.log(newArr);
 
-const arr = Object.values(obj);
-console.log(arr);
+const arr = Object.entries(obj);
+const arr1 = Object.keys(obj);
+const arr2 = Object.values(obj);
+console.log(arr , arr1 , arr2);
