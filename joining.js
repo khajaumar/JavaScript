4bb5df2d6fc499f19fcd3a719cf4df139982c43b
khajaumar <khajau460@gmail.com>
2021-08-26 04:10:53 -0700

    sumofnumberonly

--- joining.js
+++ joining.js
@@ -9,5 +9,11 @@
 // // console.log(text);
 // console.log(arry.reduce((a , b)=> a + b));
 
-arr = [2, 4, 3];
-console.log(arr.reduce((a , b)=> a * b**3, 1));
+// arr = [2, 4, 3];
+// console.log(arr.reduce((a , b)=> a * b**3, 1));
+
+const arr = [null, 2, 3, 4, "cat", {num: 25}, undefined, 2];
+
+const newArr = arr.filter((item) => typeof item === "number" && item);
+ 
+console.log(newArr.reduce((acc, num) => acc + num ));
