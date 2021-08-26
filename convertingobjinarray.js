687f2cead940e4df3c106f4e9275cca3ad4342f3
khajaumar <khajau460@gmail.com>
2021-08-26 04:56:50 -0700

    objecttoarry

---
+++ convertingobjinarray.js
@@ -0,0 +1,12 @@
+const obj = {
+    name: "khaja",
+    city: "Nanded",
+    contact: 9856236,
+};
+
+// const keys = Object.keys(obj);
+// const newArr = keys.map((key) => obj[key]);
+// console.log(newArr);
+
+const arr = Object.entries(obj);
+console.log(arr);
